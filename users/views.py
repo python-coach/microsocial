9fe4f397c7cbf6309727cf769966174175632489
python-coach <python.coach@gmail.com>
2014-09-02 12:33:11 +0300

    Easy fix.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView
 
 
 class UserProfileView(TemplateView):
