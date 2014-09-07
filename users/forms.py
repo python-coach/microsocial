8c293a4197d30ea79ea56d5fb73a64a511195b69
python-coach <python.coach@gmail.com>
2014-09-07 13:58:12 +0300

    Settings: password.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
 from django.utils.translation import ugettext
 from microsocial.forms import BootstrapFormMixin
 from users.models import User
@@ -15,3 +16,11 @@ class UserProfileForm(forms.ModelForm, BootstrapFormMixin):
         super(UserProfileForm, self).__init__(*args, **kwargs)
         BootstrapFormMixin.__init__(self)
         self.fields['birth_date'].widget.attrs['placeholder'] = ugettext(u'Введите дату в формате гггг-мм-дд')
+
+
+class UserPasswordChangeForm(PasswordChangeForm, BootstrapFormMixin):
+    def __init__(self, *args, **kwargs):
+        super(UserPasswordChangeForm, self).__init__(*args, **kwargs)
+        BootstrapFormMixin.__init__(self)
+        for field_name in ('old_password', 'new_password1', 'new_password2'):
+            self.fields[field_name] = self.fields.pop(field_name)
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 from __future__ import absolute_import
 from django.contrib import messages
+from django.contrib.auth import login, BACKEND_SESSION_KEY
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
-from users.forms import UserProfileForm
+from users.forms import UserProfileForm, UserPasswordChangeForm
 from users.models import User
 from django.utils.translation import ugettext as _
 
@@ -28,19 +29,31 @@ class UserSettingsView(TemplateView):
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        self.profile_form = UserProfileForm(request.POST or None, request.FILES or None,
-                                            prefix='profile', instance=request.user)
+        action = request.POST.get('action')
+        self.profile_form = UserProfileForm(
+            (request.POST if action == 'profile' else None),
+            (request.FILES if action == 'profile' else None),
+            prefix='profile', instance=request.user
+        )
+        self.password_form = UserPasswordChangeForm(request.user, (request.POST if action == 'password' else None),
+                                                    prefix='password')
         return super(UserSettingsView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(UserSettingsView, self).get_context_data(**kwargs)
         context['profile_form'] = self.profile_form
+        context['password_form'] = self.password_form
         return context
 
     def post(self, request, *args, **kwargs):
-        action = request.POST.get('action')
-        if action == 'profile' and self.profile_form.is_valid():
+        if self.profile_form.is_valid():
             self.profile_form.save()
             messages.success(request, _(u'Профиль успешно сохранен.'))
+            return redirect(request.path)
+        elif self.password_form.is_valid():
+            self.password_form.save()
+            request.user.backend = request.session[BACKEND_SESSION_KEY]
+            login(request, request.user)
+            messages.success(request, _(u'Пароль успешно изменен.'))
             return redirect(request.path)
         return self.get(request, *args, **kwargs)
